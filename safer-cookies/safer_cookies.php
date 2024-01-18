9ed9b6883fcc7602ebcb9f04e22d001e1fca1657
Jordan Schelew <jordan@websavers.ca>
2024-01-18 10:53:07 -0400

    Remove WordPress <= 2.5 compatibility
    It's so old it doesn't matter anymore.

--- safer-cookies/safer_cookies.php
+++ safer-cookies/safer_cookies.php
@@ -3,8 +3,8 @@
 Plugin Name: Safer Cookies
 Plugin URI: http://w-shadow.com/blog/2008/07/12/safer-cookies-plugin-for-wordpress/
 Description: Ties your login cookie to your IP address so that the cookie can't be used to access your blog from another computer.
-Version: 1.2
-Author: Janis Elsts
+Version: 2.0
+Author: Janis Elsts / Websavers.ca
 Author URI: http://w-shadow.com/blog/
 */
 
@@ -14,28 +14,13 @@ It's GPL.
 */
 
 /** ************************************************************
-	 Override existing cookie generation/validation functions
+ Override existing cookie generation/validation functions
 
  The custom functions are pretty much equivalent to the default
  ones, except using $_SERVER['REMOTE_ADDR'] as part of the hash. 	  
 ****************************************************************/
-if ( !function_exists('wp_generate_auth_cookie') ) :
+if ( !function_exists('wp_generate_auth_cookie') ){
 
-if (version_compare($wp_version, '2.6', '<')){
-	//WP 2.5 - 2.5.1
-	function wp_generate_auth_cookie($user_id, $expiration) {
-		$user = get_userdata($user_id);
-	
-		//Added the IP to the hash : 
-		$key = wp_hash($user->user_login . $expiration . $_SERVER['REMOTE_ADDR']);
-		$hash = hash_hmac('md5', $user->user_login . $expiration, $key);
-	
-		$cookie = $user->user_login . '|' . $expiration . '|' . $hash;
-	
-		return apply_filters('auth_cookie', $cookie, $user_id, $expiration);
-	}
-} else {
-	//WP 2.6+
 	function wp_generate_auth_cookie($user_id, $expiration, $scheme = 'auth') {
 		$user = get_userdata($user_id);
 
@@ -48,48 +33,11 @@ if (version_compare($wp_version, '2.6', '<')){
 	
 		return apply_filters('auth_cookie', $cookie, $user_id, $expiration, $scheme);
 	}
+
 }
 
-endif;
+if ( !function_exists('wp_validate_auth_cookie') ){
 
-if ( !function_exists('wp_validate_auth_cookie') ) :
-
-if (version_compare($wp_version, '2.6', '<')){
-	//WP 2.5 - 2.5.1
-	function wp_validate_auth_cookie($cookie = '') {
-		
-		if ( empty($cookie) ) {
-			if ( empty($_COOKIE[AUTH_COOKIE]) )
-				return false;
-			$cookie = $_COOKIE[AUTH_COOKIE];
-		}
-	
-		list($username, $expiration, $hmac) = explode('|', $cookie);
-	
-		$expired = $expiration;
-	
-		// Allow a grace period for POST and AJAX requests
-		if ( defined('DOING_AJAX') || 'POST' == $_SERVER['REQUEST_METHOD'] )
-			$expired += 3600;
-	
-		if ( $expired < time() )
-			return false;
-	
-		//Added the IP to the hash, here too : 
-		$key = wp_hash($username . $expiration . $_SERVER['REMOTE_ADDR']);
-		$hash = hash_hmac('md5', $username . $expiration, $key);
-	
-		if ( $hmac != $hash )
-			return false;
-	
-		$user = get_userdatabylogin($username);
-		if ( ! $user )
-			return false;
-	
-		return $user->ID;
-	}
-} else {
-	//WP 2.6+
 	function wp_validate_auth_cookie($cookie = '', $scheme = 'auth') {
 		if ( empty($cookie) ) {
 			switch ($scheme){
@@ -158,8 +106,7 @@ if (version_compare($wp_version, '2.6', '<')){
 	
 		return $user->ID;
 	}
+
 }
-endif;
-//*/
 
 ?>
